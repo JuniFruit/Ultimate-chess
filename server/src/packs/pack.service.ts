a3f6bdef3998d2195805089875533a34f9b28dde
JuniFruit@github.com <idiot228322@mail.ru>
2022-12-11 17:22:04 +0300

    packs api changes

--- server/src/packs/pack.service.ts
+++ server/src/packs/pack.service.ts
@@ -5,7 +5,10 @@ import { UserService } from "../user/user.service";
 export const PackService = {
 
     async create(data: IPackInfo) {
+        const spritePack = await spriteRepository.findOneBy({id: data.packPath.id});
 
+        if (!spritePack) throw new Error('Such sprite pack ID doesn\'t exist');
+        
         const newPack = await packRepository.create(data);
         return await packRepository.save(newPack);
     },
@@ -49,7 +52,7 @@ export const PackService = {
     async setInUse(userId: number, packId: number) {
         const user = await UserService.getById(userId);
         const pack = await this.getPackById(packId);
-        console.log(pack?.owner);
+
         if (!pack) throw new Error('Such resource doesn\'t exist')
         if (!user) throw new Error('User doesn\'t exist')
 

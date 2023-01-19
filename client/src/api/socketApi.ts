d6f924cc75ed9aed9fe445d7b23b4e97d9f34f0a
JuniFruit@github.com <idiot228322@mail.ru>
2023-01-19 20:21:37 +0300

    fixes

--- client/src/api/socketApi.ts
+++ client/src/api/socketApi.ts
@@ -3,7 +3,6 @@ import { IOClientEvents } from '../constants/socketIO/ClientEvents.interface';
 import { IOServerEvents } from '../constants/socketIO/ServerEvents.interface';
 import { getHandshakeAuth } from '../utils/socket.api.utils';
 
-// const URL = 'http://192.168.1.101:3001'
 const URL = '/';
 
 

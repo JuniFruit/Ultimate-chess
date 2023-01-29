949d73a0a01a64259eb98f1f9006f0c93f7bcd82
JuniFruit@github.com <idiot228322@mail.ru>
2023-01-29 12:41:42 +0300

    carousel fixes

--- client/src/components/ui/carousel/Carousel.tsx
+++ client/src/components/ui/carousel/Carousel.tsx
@@ -1,9 +1,9 @@
-import { FC, useState, useRef } from "react";
+import { FC, useRef, useState } from "react";
+import { IoArrowRedoOutline, IoArrowUndoOutline } from "react-icons/io5";
+import { useIsMobile } from "../../../hooks/useMobile";
 import { Button } from "../button/Button";
 import { ICarousel } from "./Carousel.interface";
 import styles from './Carousel.module.scss';
-import { useIsMobile } from "../../../hooks/useMobile";
-import { IoArrowRedoOutline, IoArrowUndoOutline } from "react-icons/io5";
 
 
 export const Carousel: FC<ICarousel> = ({ slides }) => {
@@ -16,17 +16,17 @@ export const Carousel: FC<ICarousel> = ({ slides }) => {
     const handleChange = (dir: 'prev' | 'next') => {
         dir === 'prev' ? setCurrent(prev => prev -= 1) : setCurrent(prev => prev += 1)
     }
-
+    console.log(slides)
     if (viewportRef.current) viewportRef.current.style.transform = `translateX(calc(${current}* -100%))`;
 
     return (
         <div className={styles.carousel_container}>
             <Button
                 onClick={() => handleChange('prev')}
-                disabled={current === -1}
+                disabled={current === 0}
                 aria-label={'previous slide'}
                 className={styles.nav_button}>
-                
+
                 <IoArrowUndoOutline />
             </Button>
             <div className={styles.carousel_overlay}>
@@ -39,7 +39,8 @@ export const Carousel: FC<ICarousel> = ({ slides }) => {
                 onClick={() => handleChange('next')}
                 className={styles.nav_button}
                 aria-label={'next slide'}
-                disabled={current === (Math.floor((slides.length / itemsPerScreen) - 1))}>
+                disabled={current === (Math.floor((slides.length / itemsPerScreen)))}
+            >
                 <IoArrowRedoOutline />
             </Button>
         </div>
